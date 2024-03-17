eebb5543c168be190020a21fd20d2f0e6796a2e0
ShellRuner <sitouamu510@gmail.com>
2024-03-17 00:57:49 +0000

    Task2 submit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("{} {}".format(0, "arguments."))
     else:
-        print("{} {}".format(len(argv) - 1, "arguments:" if len(argv) > 2
-            else "argument:"))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        if len(argv) > 2:
+            print("{} {}".format(len(argv) - 1, "arguments:"))
+        else:
+            print("{} {}".format(len(argv) - 1, "argument:"))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
