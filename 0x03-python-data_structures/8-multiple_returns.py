57d93471f17fbbf768bcba86b265a16c2b8f86e4
ShellRuner <sitouamu510@gmail.com>
2024-03-20 11:46:02 +0000

    Task8  submit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) > 0:
+    if len(sentence) > 0 and sentence is not None:
         tuple = (len(sentence), sentence[0])
         return tuple
     else:
