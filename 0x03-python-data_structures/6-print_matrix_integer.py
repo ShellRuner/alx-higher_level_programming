722981ac83ee4a9bf7d672b7e0713ec9b192727e
ShellRuner <sitouamu510@gmail.com>
2024-03-25 11:10:27 +0000

    Task6 submit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) > 0 and len(matrix[0]) > 0:
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                print("{:d}".format(matrix[i][j]), end=" ")
-            print()
+                if j == len(matrix[0]) - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+    else:
+        print()
