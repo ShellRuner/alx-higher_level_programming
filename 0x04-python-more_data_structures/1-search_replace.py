013b3180f8b2f354b45030cdf7b357d44d1467e6
ShellRuner <sitouamu510@gmail.com>
2024-03-25 18:21:53 +0000

    Task1 submit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+def search_replace(my_list, search, replace):
+    if len(my_list) > 0:
+        new_list = my_list[:]
+        for i in range(len(new_list)):
+            if new_list[i] == search:
+                new_list[i] = replace
+        return new_list
+    elif len(my_list) == 0:
+        return my_list
+    else:
+        return None
