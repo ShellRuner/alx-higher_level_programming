8d5d8667038ff3ba624798456b9fae55c9373053
ShellRuner <sitouamu510@gmail.com>
2024-03-25 17:59:44 +0000

    Task0 submit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if len(matrix) > 0 and len(matrix[0]) > 0:
+        new_matrix = []
+        for row in matrix:
+            square_row = []
+            for elemt in row:
+                square_row.append(elemt ** 2)
+            new_matrix.append(square_row)
+        return new_matrix
+    else:
+        return
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+**Task0.Squared simple
+  Write a function that compute the square value of all integers of a matrix.
+
