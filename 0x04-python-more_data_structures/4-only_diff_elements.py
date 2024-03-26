ec453cf09b9780bf61aa0593d191898257d328df
ShellRuner <sitouamu510@gmail.com>
2024-03-26 11:37:43 +0000

    Task4 submit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if not(set_1.isdisjoint(set_2)):
+    if not(set_1.isdisjoint(set_2)) or len(set_1) >= 0 and len(set_2) >= 0:
         return set_1.symmetric_difference(set_2)
     else:
         return set()
