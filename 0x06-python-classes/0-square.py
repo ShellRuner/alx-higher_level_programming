4a265407ba9b7bd6c3fdaef75a1d03caaafb6438
ShellRuner <sitouamu510@gmail.com>
2024-04-04 02:35:51 +0000

    Task0 submit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """An empty class that define a
-    square
+    """An empty class that define a square
+
+    This class doesn't have any methodes or attribute
+
+    Attributes:
+        None
     """
     pass
