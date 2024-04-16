5fba868b131829a225f1d8315b0bdcdbff72e049
ShellRuner <sitouamu510@gmail.com>
2024-04-16 22:57:24 +0000

    Task2 submit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""A class module"""
+
+
 class Square:
+    """A class Square that defines a square
+    Attributes:
+        size (int): The size of the square which is private
+    """
     def __init__(self, size=0):
+        """The __init__ method of the class square
+        Note:
+            Do not include the 'self' in the arguments
+        Args:
+            size (int): A private instance attribute
+        """
         self.__size = size
         if not isinstance(self.__size, int):
             raise TypeError('size must be an integer')
