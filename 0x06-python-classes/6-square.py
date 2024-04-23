75c8bc59ae46af938c2a7e7562eae02c6388d252
ShellRuner <sitouamu510@gmail.com>
2024-04-23 22:01:56 +0000

    Task6 submit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+"""Module that contains a class name square"""
+
+
+class Square:
+    """A class that allows to define a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """A function that allows to initialize attributes
+        Args:
+            size (int) : A private instance attribute and size
+                         of the square
+            position (int, int) : A private instance attribute
+                                  and allows to coordinate the
+                                  square
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """A public instance method that give the area of the
+        square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """A function that allows to retrieve the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A function that allows to set the size
+        Args: set  the size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        """A function that allows to retrieve the position of
+        the square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """A function that allows to set the square position
+        Args:
+            value (int, int) : set the position
+        """
+        v = value
+        if not isinstance(value, tuple) and not \
+                isinstance(v[0], int) and not isinstance(v[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """A public instance method that allows to print
+        in stdout the square with the character "#"
+        """
+        if self.__position[0] == 0 and self.__position[1] == 0:
+            if self.size == 0:
+                print("")
+            for i in range(self.size):
+                print("#" * self.size)
+
+        if self.__position[1] > 0 and self.__position[0] > 0:
+            for i in range(self.__position[1]):
+                print("")
+            if self.__size == 0:
+                print(" " * self.position[0], end="")
+                print("")
+            elif self.__size > 0:
+                for i in range(self.__size):
+                    print(" " * self.__position[0] + "#" * self.size)
+
+        if self.__position[0] == 0 and self.__position[1] > 0:
+            for i in range(self.__position[1]):
+                print("")
+
+            if self.__size == 0:
+                print("")
+            elif self.__size > 0:
+                for i in range(self.__size):
+                    print("#" * self.__size)
+
+        if self.__position[0] > 0 and self.__position[1] == 0:
+            if self.__size == 0:
+                print(" " * self.__position[1], end="")
+                print("")
+            elif self.__size > 0:
+                for i in range(self.__size):
+                    print(" " * self.__position[0] + "#" * self.__size)
