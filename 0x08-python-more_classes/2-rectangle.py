b98386e0bf71379ac7e2cc939c2f276119c54a53
ShellRuner <sitouamu510@gmail.com>
2024-04-30 15:40:19 +0000

    Task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module that contain a class Rectangle"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """A method that defines the width and the height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """allows to retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """allows to set the height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """allow to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    def area(self):
+        """allow to Calculate the area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Allows to calculate the perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
