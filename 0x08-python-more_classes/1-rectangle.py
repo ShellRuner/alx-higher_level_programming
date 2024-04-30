3bc328c87c5efc23aa54c3c12dac88cd28558a2d
ShellRuner <sitouamu510@gmail.com>
2024-04-30 13:59:46 +0000

    Task1 submit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module that contain a class Rectangle"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """A method that defines the width and the height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """allow to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """allows to retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """allows to set the height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
