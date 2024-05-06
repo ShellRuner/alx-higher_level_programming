25a4e972bbcef9b6117b6ef20e7fd0f0135fd754
ShellRuner <sitouamu510@gmail.com>
2024-05-06 13:42:29 +0000

    Task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that contain a function"""
+
+
+def write_file(filename="", text=""):
+    """A function that writes into a text file
+    an returns the number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as myfile:
+        myfile.write(text)
+        nb = myfile.tell()
+
+    return nb + 1
