de8afff7c1cc07a383acb38c8c6ba5dadf987bc8
ShellRuner <sitouamu510@gmail.com>
2024-05-06 13:05:30 +0000

    Task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A module that contain a function"""
+
+
+def read_file(filename=""):
+    """A function that reads a text file(UTF8)"""
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        print(myfile.read())
