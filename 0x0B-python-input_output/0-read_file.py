0e4a305514dcb3165b3f4eb306a6b5a71f9d3783
ShellRuner <sitouamu510@gmail.com>
2024-05-06 13:10:32 +0000

    Task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """A function that reads a text file(UTF8)"""
     with open(filename, 'r', encoding='utf-8') as myfile:
-        print(myfile.read())
+        print(myfile.read(), end='')
