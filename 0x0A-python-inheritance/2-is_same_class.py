034e7763fe6cca0fefb47af284ff2509e62c2848
ShellRuner <sitouamu510@gmail.com>
2024-05-11 15:55:22 +0000

    Task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contain a function"""
+
+
+def is_same_class(obj, a_class):
+    """A function that check if object is an instance"""
+    if (type(obj) is  a_class):
+        return True
+    else:
+        return False
