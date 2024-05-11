0187188e3e91c4afbbcffd0d049ff4f693bddc9a
ShellRuner <sitouamu510@gmail.com>
2024-05-11 16:04:34 +0000

    Task2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """A function that check if object is an instance"""
-    if (type(obj) is  a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
